1c820fd3847b383270f73344a59cccc0993d72c4
tina0420 <32476325+tina0420@users.noreply.github.com>
2019-07-22 17:11:55 +0800

    day01-HTML

---
+++ day01/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eating</title>
+</head>
+<body>
+<ul>
+
+<li>項目1</li>
+<li>項目2</li>
+<li>項目3</li>
+
+</ul>
+<ol>
+
+<li>項目1</li>
+<li>項目2</li>
+<li>項目3</li>
+
+</ol>
+<img src="https://picsum.photos/id/1043/600/400
+" alt="senery">
+<img src="./street.jpg" alt="" width="600px">
+<!-- _black 會開新視窗-->
+    <a href="https://www.google.com" target="_black">Google網站</a>
+    <a href="./about.php">About</a>
+    <h1>Let's eating!</h1>
+    <h2>title 2</h2>
+    <hr>
+
+    <!-- lorem20 預留20字的空間-->
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nihil quos voluptate asperiores amet ipsam dolorem sapiente saepe voluptatibus ex?</p>
+</body>
+</html>
